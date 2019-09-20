0459a0efa83a6740819c7196e886188ea7b2d643
nabil <djanfar.nabil@gmail.com>
2019-09-20 15:14:33 +0200

    white and red menu slide

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 var btn = document.querySelector('.burger');
 var nav = document.querySelector('.nav');
+var nav2 = document.querySelector('.nav2');
 
 btn.onclick = function(){
-	nav.classList.toggle('nav_open');
 	btn.classList.toggle('anime');
-}
+	nav.classList.toggle('nav_open');
+	nav2.classList.toggle('nav2_open');
+		}
+
